3dbd851c8e21bda8edf59a25fbf201908d0f6652
JassonLuiz <jassonluizjr@hotmail.com>
2022-03-10 16:42:51 -0300

    Loga no sistema e obtem o token com o formulário de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,7 +22,15 @@ export class LoginComponent {
   ) { }
 
   onSubmit(){
-    this.router.navigate(['/home']);
+
+    this.authService
+          .tentaLogin(this.username, this.password)
+          .subscribe(response => {
+            console.log(response);
+            this.router.navigate(['/home']);
+          }, errorResponse => {
+            this.errors = ['Usuário e/ou senha incorreto(s).']
+          })
   }
   
   cadastrandoUser(event: any){
